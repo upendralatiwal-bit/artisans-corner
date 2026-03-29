67ffac511a5279a29d5742197a129228661e6a8b
Upendra Kumar <upendra.latiwal@gmail.com>
2026-03-29 15:15:30 +0530

    force redeploy check

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
+console.log("NEW CODE RUNNING ✅");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
+
 
 // 🔗 Routes
 const authRoutes = require("./routes/authRoutes");
